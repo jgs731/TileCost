8da015b7993d47c4b3777136911265132757b2ed
jgs731 <jgs73@live.co.uk>
2022-02-15 18:51:13 +0000

    Completed program with stretch task 1
    - Cost is returned in the Console when the user enters a defined area
    - Stretch task includes calculation for a triangular room

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TileCost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width;
+            int height;
+            int cost_per_unit = 6;
+            int area;
+            double total_cost;
+            string room_type;
+
+            Console.Write("What is the shape of the room? ");
+            room_type = Console.ReadLine();
+            Console.Write("Enter the width: ");
+            width = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the length: ");
+            height = Convert.ToInt32(Console.ReadLine());
+            if (room_type == "triangle")
+            {
+                area = (width * height) / 2;
+            }
+            else
+            {
+                area = width * height;
+            }
+            total_cost = cost_per_unit * area;
+            Console.Write($"Quote for carpet: £{total_cost}");
+        }
+    }
+}
